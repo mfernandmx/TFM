eff990539ca34c19c70acfc6067d4fa08a00113e
marfersel <marcosfsellers@gmail.com>
2018-10-30 13:41:38 +0100

    Add form select options. Add favicon.

--- scripts/interface.py
+++ scripts/interface.py
@@ -2,14 +2,20 @@
 # -*- coding: utf-8 -*-
 
 from flask import Flask, render_template, flash, request
-from wtforms import Form, validators, StringField
+from wtforms import Form, validators, StringField, SelectField
 
 app = Flask(__name__)
 app.config.from_object(__name__)
 app.config['SECRET_KEY'] = '7d441f27d441f27567d441f2b6176a'
 
 class ReusableForm(Form):
-    name = StringField('Name:', validators=[validators.required()])
+    # TODO: Añadir sugerencia de portales
+    portal1 = StringField('URL Portal 1:', validators=[validators.required()])
+    type1 = SelectField('Portal Type 1', choices=[('ckan', 'ckan'), ('socrata', 'socrata')])
+
+    portal2 = StringField('URL Portal 2:', validators=[validators.required()])
+    type2 = SelectField('Portal Type 2', choices=[('ckan', 'ckan'), ('socrata', 'socrata')])
+
 
 @app.errorhandler(404)
 def page_not_found():
@@ -22,12 +28,16 @@ def home():
     print(form.errors)
 
     if request.method == 'POST':
-        name = request.form['name']
-        print(name)
+        portal1 = request.form['portal1']
+        portal2 = request.form['portal2']
+        type1 = request.form['type1']
+        type2 = request.form['type2']
+        print(portal1, portal2, type1, type2)
 
         if form.validate():
             # Save the comment here.
-            flash('Hello ' + name)
+            flash('Portal 1: ' + portal1 + ' (' + type1 + ')')
+            flash('Portal 2: ' + portal2 + ' (' + type2 + ')')
         else:
             flash('All the form fields are required. ')
 
